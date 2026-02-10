a37125f21da3bfa9c66d7eba08362a0f5481ab92
Efeatagul <efeataguljob@gmail.com>
2026-02-10 23:37:18 +0300

    Refactor WType and WValue classes for clarity

--- Types.cs
+++ Types.cs
@@ -4,80 +4,124 @@ using System.Collections.Generic;
 
 namespace WSharp
 {
-   
+    
     public enum WType
     {
-        Null,
         Number,
         String,
-        Function
-    }
-
-  
-    public class WValue
-    {
-        public object RawValue;
-        public WType Type;
-
-       
-        public bool IsSystemFunction = false;
-        public Func<List<WValue>, WValue> SystemFunction;
-
-        public WValue(object value, WType type)
-        {
-            RawValue = value;
-            Type = type;
-        }
-
-        public WValue(Func<List<WValue>, WValue> function)
-        {
-            Type = WType.Function;
-            IsSystemFunction = true;
-            SystemFunction = function;
-            RawValue = "<native fn>";
-        }
-
-        public double AsNumber() => RawValue is double d ? d : 0;
-        public string AsString() => RawValue?.ToString() ?? "";
-    }
-
-    public class WNumber : WValue
-    {
-        public WNumber(double value) : base(value, WType.Number) { }
-    }
-
-    public class WString : WValue
-    {
-        public WString(string value) : base(value, WType.String) { }
-    }
-
-    public class WNull : WValue
-    {
-        public WNull() : base(null, WType.Null) { }
+        Bool,
+        Function,
+        List,
+        Dict,
+        Null
     }
 
     
-    public class Scope
+    public interface IWCallable
+    {
+        int Arity(); 
+        WValue Call(Interpreter interpreter, List<WValue> arguments);
+    }
+
+    
+    public class WValue
+    {
+        public object Value { get; set; }
+        public WType Type { get; set; }
+
+        public WValue(object value)
+        {
+            Value = value;
+            if (value == null) Type = WType.Null;
+            else if (value is double) Type = WType.Number;
+            else if (value is bool) Type = WType.Bool;
+            else if (value is string) Type = WType.String;
+            else if (value is IWCallable) Type = WType.Function;
+            else if (value is List<object>) Type = WType.List;
+            else if (value is Dictionary<string, WValue>) Type = WType.Dict;
+            else if (value is int || value is float || value is decimal)
+            {
+                Value = Convert.ToDouble(value);
+                Type = WType.Number;
+            }
+            else throw new Exception($"Bilinmeyen tip: {value.GetType()}");
+        }
+
+        public bool AsBoolean()
+        {
+            if (Value is bool b) return b;
+            if (Value is double d) return d != 0;
+            return Value != null;
+        }
+
+        public double AsNumber() => (double)Value;
+        public string AsString() => (string)Value;
+        public List<object> AsList() => (List<object>)Value;
+        public Dictionary<string, WValue> AsDict() => (Dictionary<string, WValue>)Value;
+        public bool IsCallable() => Type == WType.Function;
+        public IWCallable AsCallable() => (IWCallable)Value;
+
+        public override string ToString()
+        {
+            if (Type == WType.Number) return ((double)Value).ToString();
+            if (Type == WType.String) return (string)Value;
+            
+            
+            if (Type == WType.List)
+            {
+                var list = (List<object>)Value;
+                return "[" + string.Join(", ", list) + "]";
+            }
+            
+            
+            if (Type == WType.Dict)
+            {
+                var dict = (Dictionary<string, WValue>)Value;
+                var pairs = new List<string>();
+                foreach (var kvp in dict)
+                {
+                    pairs.Add($"{kvp.Key}: {kvp.Value}");
+                }
+                return "{" + string.Join(", ", pairs) + "}";
+            }
+            
+            return Value?.ToString() ?? "null";
+        }
+    }
+
+    
+    public class Environment
     {
         private readonly Dictionary<string, WValue> _values = new Dictionary<string, WValue>();
-        public Scope Enclosing; 
+        public readonly Environment Enclosing; 
 
-        public Scope() { Enclosing = null; }
-        public Scope(Scope enclosing) { Enclosing = enclosing; }
+        public Environment()
+        {
+            Enclosing = null;
+        }
 
+        public Environment(Environment enclosing)
+        {
+            Enclosing = enclosing;
+        }
+
+       
         public void Define(string name, WValue value)
         {
             _values[name] = value;
         }
 
+        
         public WValue Get(Token name)
         {
             if (_values.ContainsKey(name.Value)) return _values[name.Value];
+
             if (Enclosing != null) return Enclosing.Get(name);
 
-            throw new Exception($"Tanımsız değişken: '{name.Value}'.");
+            throw new Exception($"Tanimsiz degisken: '{name.Value}'.");
         }
 
+        
         public void Assign(Token name, WValue value)
         {
             if (_values.ContainsKey(name.Value))
@@ -92,7 +136,101 @@ namespace WSharp
                 return;
             }
 
-            throw new Exception($"Tanımsız değişkene atama yapılamaz: '{name.Value}'.");
+            throw new Exception($"Tanimsiz degiskene atama yapilamaz: '{name.Value}'.");
         }
+    }
+
+   
+    public class WFunction : IWCallable
+    {
+        private readonly FunctionStmt _declaration;
+        private readonly Environment _closure;
+
+        public WFunction(FunctionStmt declaration, Environment closure)
+        {
+            _declaration = declaration;
+            _closure = closure;
+        }
+
+        public int Arity() => _declaration.Params.Count;
+
+        public WValue Call(Interpreter interpreter, List<WValue> arguments)
+        {
+            
+            Environment environment = new Environment(_closure);
+
+            for (int i = 0; i < _declaration.Params.Count; i++)
+            {
+                environment.Define(_declaration.Params[i].Value, arguments[i]);
+            }
+
+            
+            try
+            {
+                interpreter.ExecuteBlock(_declaration.Body, environment);
+            }
+            catch (ReturnException returnValue)
+            {
+                return returnValue.Value;
+            }
+
+            return new WValue(null);
+        }
+
+        public override string ToString() => $"<fn {_declaration.Name.Value}>";
+    }
+
+    
+    public class WLambda : IWCallable
+    {
+        private readonly LambdaExpr _declaration;
+        private readonly Environment _closure;
+
+        public WLambda(LambdaExpr declaration, Environment closure)
+        {
+            _declaration = declaration;
+            _closure = closure;
+        }
+
+        public int Arity() => _declaration.Parameters.Count;
+
+        public WValue Call(Interpreter interpreter, List<WValue> arguments)
+        {
+            Environment environment = new Environment(_closure);
+            for (int i = 0; i < _declaration.Parameters.Count; i++)
+            {
+                environment.Define(_declaration.Parameters[i].Value, arguments[i]);
+            }
+
+            
+            if (_declaration.Body is ExpressionStmt exprStmt)
+            {
+                return interpreter.Evaluate(exprStmt.Expression);
+            }
+            
+            
+            if (_declaration.Body is BlockStmt blockStmt)
+            {
+                try
+                {
+                    interpreter.ExecuteBlock(blockStmt.Statements, environment);
+                }
+                catch (ReturnException returnValue)
+                {
+                    return returnValue.Value;
+                }
+            }
+
+            return new WValue(null);
+        }
+
+        public override string ToString() => "<fn lambda>";
+    }
+
+    
+    public class ReturnException : Exception
+    {
+        public WValue Value;
+        public ReturnException(WValue value) { Value = value; }
     }
 }
